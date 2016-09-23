e74c61e13a34e4a9c92ce7c28e65086b86172cb2
qho.queenie <qho.queenieho@gmail.com>
2016-09-23 14:17:19 -0700

    test

--- pylotVenv/bin/activate.fish
+++ pylotVenv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/queenieho/Desktop/WebFundamental/Python/Flask/CodingDojo_Python/myEnvironments/login/pylotVenv"
+set -gx VIRTUAL_ENV "/Users/queenieho/Desktop/WebFundamental/Python/Flask/CodingDojo_Python/myEnvironments/loginBackup/pylotVenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
